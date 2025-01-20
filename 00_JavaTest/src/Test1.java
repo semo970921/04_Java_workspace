0d9ad886a0a2eeb71ab8404743f8f03bb55e5b27
SeungWon <semo970921@naver.com>
2025-01-20 21:01:44 +0900

    20250120_Java Test

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,9 @@
+// JS와 주석 동일
+
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("안녕");
+        System.out.println("타자 연습좀 해");
+        System.out.println("메롱");
+    }
+}
