058d88fe7bc74de7f32989f3c1b9c0f1aba5b7f7
SeungWon <semo970921@naver.com>
2025-01-21 11:53:50 +0900

    20250121_idea폴더 추적 삭제

---
+++ 01_Variable/src/ex/VariableEx2.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class VariableEx2 {
+    public static void main(String[] args) {
+        System.out.println("[자동 형변환 예시1]");
+
+        int a1 = 12;
+        double b1 = 1.3;
+        // int result1 = a1 + b1;
+        double result1 = a1 + b1;
+        System.out.println("result1 = " + result1);
+    }
+}
