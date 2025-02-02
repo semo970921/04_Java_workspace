04c032e07993d9c741d1bd873f552c988aa1ca3d
SeungWon <semo970921@naver.com>
2025-02-02 17:02:43 +0900

    생성자로 실행(commi commitãit -m 오타로 인한 수정)

--- 05_class_object/src/abstraction/ConstructorRun.java
+++ 05_class_object/src/abstraction/ConstructorRun.java
@@ -14,7 +14,6 @@ public class ConstructorRun {
         Student std2 = new Student("202127897",  "유리");
         System.out.println(std2.toString());
 
-
         // Student 객체 필드를 모두 초기화 하는 생성자 이용
         Student std3 = new Student("202121943", "짱구", 50 , 40, 30, 59);
         System.out.println(std3.toString());
